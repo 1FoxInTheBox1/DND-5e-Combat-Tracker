3e726e92205dc9552135899e204aa64510e4e10e
rhit-yangnj <njy0809@gmail.com>
2024-04-15 20:28:17 -0400

    Cancel button on popups

--- add_player_popup.gd
+++ add_player_popup.gd
@@ -3,5 +3,9 @@ extends Popup
 signal window_closed(player_name : String, initiative : int)
 
 func _on_done_pressed() -> void:
-	window_closed.emit($VBoxContainer/Input.text, $VBoxContainer/Initiative.value)
+	window_closed.emit($MarginContainer/VBoxContainer/Input.text, $MarginContainer/VBoxContainer/Initiative.value)
+	hide()
+
+
+func _on_cancel_pressed() -> void:
 	hide()
--- save_encounter_popup.gd
+++ save_encounter_popup.gd
@@ -3,10 +3,14 @@ extends Popup
 signal window_closed(filename : String)
 
 func _on_done_pressed() -> void:
-	window_closed.emit($VBoxContainer/Input.text);
+	window_closed.emit($MarginContainer/VBoxContainer/Input.text);
 	hide()
 
 
 func _on_input_text_submitted(new_text: String) -> void:
-	window_closed.emit($VBoxContainer/Input.text);
+	window_closed.emit($MarginContainer/VBoxContainer/Input.text);
+	hide()
+
+
+func _on_cancel_pressed() -> void:
 	hide()
